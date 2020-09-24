a6d86dde93640abbaab2bdfd6ec2fff88d2c3a1d
briankwong65 <briankwong98@gmail.com>
2020-09-24 23:36:49 +0800

    Delete unwanted lines

--- src/controller/report-controller.js
+++ src/controller/report-controller.js
@@ -1,9 +1,6 @@
 const reportService = require('../services/report-service');
 
 async function generateReport(req, res) {
-  // step - function
-  const inputFile = req.files.foo;
-  console.log(inputFile);
   let data = 'success';
   data = await reportService.convertExceltoJSON('GOOD!');
   return res.status(200).send(data);
